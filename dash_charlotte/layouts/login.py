4912ec8ea4b285463b2b251a099d831b1332a626
Gustavo Furtado <gustavofurtado2@gmail.com>
2022-04-03 12:45:03 -0300

    🎨 Expand `LoginPage` structure

--- dash_charlotte/layouts/login.py
+++ dash_charlotte/layouts/login.py
@@ -1,14 +1,20 @@
-from typing import List
+from typing import Optional
+from uuid import uuid4
 
-from dash import (
-    html,
-    dcc
-)
+from dash import html, dcc
 import dash_bootstrap_components as dbc
 
 
 
 class SvgImage(html.Div):
+    """A panel containing an image.
+
+    Parameters
+    ----------
+    src : str
+        Source of the image.
+    
+    """
 
     def __init__(
             self,
@@ -29,15 +35,60 @@ class SvgImage(html.Div):
 
 
 class LoginForm(html.Div):
+    """Form to be added in the `LoginPage` component.
+
+    Parameters
+    ----------
+    id : str, optional
+        Component id.
+    title_text : str, default='Log in'
+        Text displayed at the top of the form.
+    title_color : str, default='shade6'
+        Color class of the title text.
+    subtitle_text : str, default='Enter your username and password'
+        Text display right below the title text.
+    subtitle_color : str, default='shade4'
+        Color class of the subtitle text.
+    email_placeholder : str, default='Username'
+        Placeholder text of the email input.
+    password_placeholder : str, default='Password'
+        Placeholder text of the password input.
+    button_text : str, default='Log in'
+        Text of the submit button.
+    button_color : str, default='blue'
+        Color class of the submit button.
+
+    Components IDs
+    --------------
+    {id}
+        Main content (children).
+    {id}--title
+        Title text element.
+    {id}--subtitle
+        Subtitle text element.
+    {id}--email
+        Value of the email input field.
+    {id}--password
+        Value of the password input field.
+    {id}--button
+        Submit button.
+
+    """
 
     def __init__(
             self,
+            id: Optional[str] = None,
             title_text: str = 'Log in',
+            title_color: str = 'shade6',
+            subtitle_text: str = 'Enter your username and password',
+            subtitle_color: str = 'shade4',
             email_placeholder: str = 'Username',
             password_placeholder: str = 'Password',
             button_text: str = 'Log in',
             button_color: str = 'blue'
         ):
+
+        self.id = id or str(uuid4())
 
         super().__init__(
             style = {
@@ -50,7 +101,12 @@ class LoginForm(html.Div):
                 className = 'sign-in-form',
                 children = [
                     self.title_text(
-                        text = title_text
+                        text = title_text,
+                        color = title_color
+                    ),
+                    self.subtitle_text(
+                        text = subtitle_text,
+                        color = subtitle_color
                     ),
                     self.email_input(
                         placeholder = email_placeholder
@@ -67,14 +123,20 @@ class LoginForm(html.Div):
         )
 
 
-    def title_text(self, text) -> html.H2:
+    def title_text(self, text, color:str) -> html.H2:
         return html.H2(
-            className = 'shade6',
+            className = f'{color} mb-2',
+            id = f'{self.id}--title',
+            children = text
+        )
+
+
+    def subtitle_text(self, text, color:str) -> html.H5:
+        return html.H5(
+            className = f'{color} mb-4',
+            id = f'{self.id}--subtitle',
             children = text,
-            style = {
-                'font-size': '2.2rem',
-                'margin-bottom': '10px',
-            }
+            style = {'text-align': 'center'}
         )
 
 
@@ -86,6 +148,7 @@ class LoginForm(html.Div):
                     className = 'fas fa-user'
                 ),
                 dcc.Input(
+                    id = f'{self.id}--email',
                     type = 'text',
                     placeholder = placeholder
                 )
@@ -101,6 +164,7 @@ class LoginForm(html.Div):
                     className = 'fas fa-lock'
                 ),
                 dcc.Input(
+                    id = f'{self.id}--password',
                     type = 'password',
                     placeholder = placeholder
                 )
@@ -110,6 +174,7 @@ class LoginForm(html.Div):
 
     def submit_button(self, text:str, color:str) -> dcc.Input:
         return dcc.Input(
+            id = f'{self.id}--button',
             type = 'submit',
             value = text,
             className = f'login-button bg-{color} bg-hover-{color}'
@@ -118,18 +183,53 @@ class LoginForm(html.Div):
 
 
 class LoginPage(dbc.Container):
+    """LoginPage layout.
 
-    def __init__(self, left_panel, right_panel):
+    Parameters
+    ----------
+    left_panel : list of Dash components
+        Content of the left panel.
+    right_panel : list of Dash components
+        Content of the right panel.
+    left_panel_width : int, default = 6
+        Width of the left panel, from 1 to 12.
+    right_panel_width : int, default = 6
+        Width of the right panel, from 1 to 12.
+    id : str, optional
+        Component id.
+
+    Attributes
+    ----------
+    {id}-left-panel : list of Dash components
+        Content of the left panel.
+    {id}-right-panel : list of Dash components
+        Content of the right panel.
+
+    """
+
+    def __init__(
+            self,
+            left_panel,
+            right_panel,
+            left_panel_width: int = 6,
+            right_panel_width: int = 6,
+            id: Optional[str] = None
+        ):
+
+        id = id or str(uuid4())
+
         super().__init__(
             children = dbc.Row([
                 dbc.Col(
-                    left_panel,
-                    width = 6,
+                    children = left_panel,
+                    id = f'{id}--left-panel',
+                    width = left_panel_width,
                     style = {'min-height': '100%'}
                 ),
                 dbc.Col(
-                    right_panel,
-                    width = 6,
+                    children = right_panel,
+                    id = f'{id}--right-panel',
+                    width = right_panel_width,
                     style = {'min-height': '100%'}
                 )
             ],
--- pages/login_page.py
+++ pages/login_page.py
@@ -1,4 +1,10 @@
-from dash import register_page
+from dash import (
+    register_page,
+    callback,
+    Output,
+    Input,
+    State
+)
 
 from dash_charlotte.layouts import login
 
@@ -12,7 +18,26 @@ register_page(
 
 
 
-layout = login.LoginPage(
-    left_panel = login.SvgImage(src='/assets/img/register.svg'),
-    right_panel = login.LoginForm()
+left_panel = login.LoginForm(
+    id = 'login-form',
 )
+
+right_panel = login.SvgImage(
+    src = '/assets/img/register.svg'
+)
+
+layout = login.LoginPage(
+    left_panel = left_panel,
+    right_panel = right_panel
+)
+
+
+
+@callback(
+    Output('login-form--subtitle', 'children'),
+    Input('login-form--button', 'n_clicks'),
+    Input('login-form--password', 'value'),
+    State('login-form--email', 'value'),
+    prevent_initial_call = True)
+def login(submit_click, password, email):
+    return 'Logged in!'

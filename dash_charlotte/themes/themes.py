db4d49dbd2f274563f7782782097650401434a9b
Gustavo Furtado <gustavofurtado2@gmail.com>
2022-04-17 11:11:46 -0300

    ⚡️ Update CDN link to minified version

--- dash_charlotte/themes/themes.py
+++ dash_charlotte/themes/themes.py
@@ -6,6 +6,6 @@
 
 ROOT = r'https://gusfurtado.github.io/dash-charlotte/css_themes/'
 
-CHARLOTTE_DARK = ROOT + 'charlotte_dark.css'
-CHARLOTTE_LIGHT = ROOT + 'charlotte_light.css'
-DRACULA = ROOT + 'dracula.css'
+CHARLOTTE_DARK = ROOT + 'charlotte_dark.min.css'
+CHARLOTTE_LIGHT = ROOT + 'charlotte_light.min.css'
+DRACULA = ROOT + 'dracula.min.css'

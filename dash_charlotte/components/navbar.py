9764cf2f690d71e9cf8307e0b3a5a4f6f04dddaa
Gustavo Furtado <gustavofurtado2@gmail.com>
2022-06-13 21:58:54 -0300

    ✨ Add `children` attribute to `Navbar`

--- dash_charlotte/components/navbar.py
+++ dash_charlotte/components/navbar.py
@@ -10,6 +10,7 @@ class Navbar(html.Nav):
     def __init__(
             self,
             title: Union[str, Component],
+            children: Optional[Component] = None,
             id: Optional[str] = None
         ):
         """Navbar components.
@@ -18,10 +19,21 @@ class Navbar(html.Nav):
         ----------
         title : str | Dash Component
             Text displayed inside the navbar.
+        children : Dash Component, optional
+            Content in the right of the navbar.
         id : str, optional
             Component's ID.
+
+        Components IDs
+        --------------
+        {id}--title
+            Navbar title.
+        {id}--children
+            Content in the right of the navbar.
         
         """
+
+        id = id or str(uuid.uuid4())
 
         super().__init__(
             className = 'home-content shade7',
@@ -31,9 +43,23 @@ class Navbar(html.Nav):
                     id = 'open-drawer'
                 ),
                 html.Span(
-                    className = 'text',
-                    children = title,
-                    id = id or str(uuid.uuid4())
+                    children = [
+                        html.Span(
+                            className = 'text',
+                            children = title,
+                            id = f'{id}--title'
+                        ),
+                        html.Span(
+                            className = 'text',
+                            children = children,
+                            id = f'{id}--children'
+                        )
+                    ],
+                    style = {
+                        'width': '100%',
+                        'display': 'flex',
+                        'justify-content': 'space-between'
+                    }
                 )
             ]
         )

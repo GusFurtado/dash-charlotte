205f7515988903725083a5e800686dba1d6cae31
Gustavo Furtado <gustavofurtado2@gmail.com>
2022-10-29 22:23:06 -0300

    ✨ New log-in system

--- {{cookiecutter.app_name}}/app.py
+++ {{cookiecutter.app_name}}/app.py
@@ -1,6 +1,19 @@
-from dash import Dash, page_container
-import dash_bootstrap_components as dbc
+# Native Python packages
+import logging
+from socket import gethostname
 
+# Web stuff
+from dash import (
+    Dash,
+    Input,
+    Output,
+    no_update,
+    page_container
+)
+from flask import Flask
+{% if cookiecutter.environment == "windows" %}from waitress import serve
+{% endif %}
+# Charlotte components
 from components import (
     Dashboard,
     Drawer,
@@ -10,20 +23,34 @@ from components import (
     DrawerFooter,
     Navbar
 )
+from pages.login.login_auth import LoginAuth
+
+
+# Create server with secret key
+server = Flask(__name__)
+server.secret_key = 'SECRET_KEY'
 
 
 
+# Instanciate Dash app
 app = Dash(
     name = __name__,
+    server = server,
     title = '{{cookiecutter.app_name}}',
     use_pages = True,
-    external_stylesheets = [
-        dbc.themes.BOOTSTRAP
-    ]
+    update_title = 'Atualizando...',
 )
 
 
 
+# Instanciate Error Handler
+file_handler = logging.FileHandler('data/errorlog.log')
+file_handler.setLevel(logging.WARNING)
+server.logger.addHandler(file_handler)
+
+
+
+# Create Drawer
 nav_links = [
     DrawerSingleItem(
         name = 'Login',
@@ -58,10 +85,13 @@ nav_links = [
 
 
 
+# Create Dashboard Layout
 app.layout = Dashboard(
     children = page_container,
+    id = 'dashboard',
     navbar = Navbar(
-        title = '{{cookiecutter.app_name}}'
+        title = 'My Web App',
+        id = 'dashboard-navbar'
     ),
     drawer = Drawer(
         menu = nav_links,
@@ -72,9 +102,65 @@ app.layout = Dashboard(
 
 
 
-server = app.server
+@app.callback(
+    Output('dashboard--location', 'href'),
+    Output('dashboard-navbar--title', 'children'),
+    Input('dashboard--location', 'pathname'))
+def init_app(path):
+    """Redireciona para tela de login caso usuário não esteja logado.
+    
+    Inputs
+    ------
+    dashboard--location.pathname
+        Página que o usuário está tentando acessar.
+
+    Outputs
+    -------
+    dashboard--location.href
+        Endereço da tela de login para a qual o usuário seja redirecionado.
+    dashboard-navbar--title.children
+        Título do relatório na Navbar.
+
+    """
+
+    # Get current HTTP status
+    status = LoginAuth.check_status()
+
+    # Change Navbar title if user is authenticated
+    if (status==200) or (path=='/login'):
+        try:
+            return no_update, {
+                '/login': 'User Authentication',
+                '/page1': 'Example Page 1'
+            }[path]
+        except KeyError:
+            return no_update, '{{cookiecutter.app_name}}'
+
+    # Change Navbar title if user had access denied
+    elif path=='/forbidden':
+        return no_update, 'Usuário não autorizado'
+
+    # Go to 403 page
+    elif status==403:
+        return f'http://{gethostname()}:{{cookiecutter.port}}/forbidden', no_update
+
+    # Go to login page
+    elif status==401:
+        return f'http://{gethostname()}:{{cookiecutter.port}}/login?page={path}', no_update
+
+
+{% if cookiecutter.environment == "windows" %}
+# Run app
 if __name__ == '__main__':
-    app.run_server(
+    serve(
+        app = app.server,
+        port = {{cookiecutter.port}}
+    )
+{% elif cookiecutter.environment == "dev" %}
+# Run app
+if __name__ == '__main__':
+    app.run(
         host = '0.0.0.0',
         port = {{cookiecutter.port}}
     )
+{% endif %}

80c3d4edd42be2f613c2604e130b5904f73c4511
Gustavo Furtado <gustavofurtado2@gmail.com>
2022-10-29 22:21:58 -0300

    🔧 Move `python-version` to `post_gen_project`

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,5 +1,14 @@
 import os
+import sys
 
 
+
+# Remove `Procfile` if OS is not linux
 if '{{cookiecutter.environment}}' != 'linux':
     os.remove(os.path.join(os.getcwd(), 'Procfile'))
+
+
+
+# Add python version to `runtime.txt`
+with open('runtime.txt', 'w') as rt:
+    rt.write(f'python-{sys.version.split(" ")[0]}')
